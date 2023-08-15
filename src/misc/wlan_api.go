afc5ffcfb24b9b8dea7b5c796dbc2a9dc69c319f
Saran Sappa <saran.sappa@arista.com>
2023-08-15 00:09:13 -0700

    wlan_api.go: add support for specifying interface guid and print descriptive errors

--- src/misc/wlan_api.go
+++ src/misc/wlan_api.go
@@ -34,11 +34,12 @@ func main() {
 	ssidName := flag.String("ssidname", "SSID_Test", "SSID name to set userprofile.")
 	userName := flag.String("username", "Username_Test", "Username for 1x authentication.PEAP/MSCHAPv2")
 	passWord := flag.String("password", "Password_Test", "Password for 1x authentication.PEAP/MSCAHPv2")
+	guid := flag.String("guid", "{00000000-0000-0000-0000-000000000000}", "Interface GUID")
 	flag.Parse()
 
 	// Throw error and return if default values are present
 	if *ssidName == "SSID_Test" || *userName == "Username_Test" || *passWord == "Password_Test" {
-		fmt.Printf("Default values are not accepted for SSID or Username or Password.")
+		fmt.Printf("ERROR: Default values are not accepted for SSID or Username or Password.")
 		return
 	}
 	// Replace with username and password provided by command line values.
@@ -51,22 +52,42 @@ func main() {
 	// Create session handle for api interaction.
 	session, _, err := CreateClientSession(2)
 	if err != nil {
+		fmt.Printf("ERROR: PEAP credentials are not configured because of failure in client session creation due to %q", err)
 		return
 	}
 	defer session.Close()
 
 	// Get all available interface and iterate over all of them to set eapuser profile only
 	// if interface is in connected state or disconnected state.
+	// Also match interface GUID if provided
+	areCredentialsSet := false
+	errorDisplayed := false
 	iil, err := session.Interfaces()
 	defer iil.Close()
 	for i := 0; i < int(iil.NumberOfItems); i++ {
 		iif := iil.Item(uint32(i))
-		if iif.State == InterfaceStateConnected || iif.State == InterfaceStateDisconnected {
+		if iif.State == InterfaceStateConnected || iif.State == InterfaceStateDisconnected && (iif.InterfaceGUID.String() == *guid || *guid == "{00000000-0000-0000-0000-000000000000}") {
 			err := session.SetProfileEAPXMLUserData(&iif.InterfaceGUID, *ssidName, 1, string(fileBytes[:]))
-			if err != nil {
+			result := ""
+			if err == nil {
+				result = fmt.Sprintf("SUCCESS: PEAP credentials are configured for interface with GUID %s", iif.InterfaceGUID.String())
+				areCredentialsSet = true
+			} else {
+				result = fmt.Sprintf("ERROR: PEAP credentials are not configured for interface with GUID %s due to %q", iif.InterfaceGUID.String(), err)
+				errorDisplayed = true
+			}
+			fmt.Println(result)
+
+			// Break from the loop and return if interface GUID is provided
+			if *guid != "{00000000-0000-0000-0000-000000000000}" {
 				return
 			}
 		}
 	}
+
+	if !areCredentialsSet && !errorDisplayed {
+		fmt.Printf("ERROR: PEAP credentials are not configured. Possible reason is incorrect interface GUID provided.")
+	}
+
 	return
 }
